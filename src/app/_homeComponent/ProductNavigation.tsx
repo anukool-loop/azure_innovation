262c92307519dcce243f903b692b87907ee18fb5
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-07-28 16:11:16 +0530

    logo has been completed

--- src/app/_homeComponent/ProductNavigation.tsx
+++ src/app/_homeComponent/ProductNavigation.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export const ProductNavigation: React.FC = () => {
   return (
-    <nav className="flex flex-wrap gap-10 px-[100px] py-5 w-full text-xs font-light text-white bg-black border-b border-white border-opacity-10 max-md:px-5 max-md:max-w-full">
-      <div className="flex gap-2">
+    <nav className="flex flex-wrap items-center gap-10 px-[100px] py-5 w-full text-xs font-light text-white bg-black border-b border-white border-opacity-10 max-md:px-5 max-md:max-w-full">
+      <div className="flex gap-2  items-center">
         <img
           src="https://api.builder.io/api/v1/image/assets/TEMP/933d96863eb67b9f0e5e1962dbd8b111241a9831?placeholderIfAbsent=true&apiKey=3332ba944108427ea5002522aefee114"
           alt="Authorized reseller badge"
@@ -12,7 +12,7 @@ export const ProductNavigation: React.FC = () => {
         />
         <span className="basis-auto">Authorised Reseller</span>
       </div>
-      <div className="flex flex-wrap gap-10 items-center self-start max-md:max-w-full">
+      <div className="flex flex-wrap gap-10 items-center  max-md:max-w-full ">
         <a href="#" className="self-stretch my-auto">
           Mac
         </a>
